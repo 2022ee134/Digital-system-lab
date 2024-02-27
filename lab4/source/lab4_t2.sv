ba41f53c9e86e748639e3dc81b7323d0b8edca5b
2022ee134 <159075214+2022ee134@users.noreply.github.com>
2024-02-27 15:42:41 +0500

    Add files via upload

--- lab4/source/lab4_t2.sv
+++ lab4/source/lab4_t2.sv
@@ -2,11 +2,11 @@ module lab4_t2(output logic a,b,c,d,e,f,g,a1,a2,a3,a4,a5,a6,a7,a8,
 input logic [3:0]num,[2:0]sel);
 always_comb begin 
     a=((~num[3])&&(~num[2])&&(~num[1])&&(num[0]))||((~num[3])&&(num[2])&&(~num[1])&&(~num[0]))||((num[3])&&(num[2])&&(~num[1])&&(num[0]))||((num[3])&&(~num[2])&&(num[1])&&(num[0]));
-    b=((num[3])&&(num[2])&&(~num[0]))||((num[2])&&(num[1])&&(~num[0]))||((~num[3])&&(num[2])&&(~num[1])&&(~num[0]))||((num[3])&&(num[1])&&(num[0]));
+    b=((num[3])&&(num[2])&&(~num[0]))||((num[2])&&(num[1])&&(~num[0]))||((~num[3])&&(num[2])&&(~num[1])&&(num[0]))||((num[3])&&(num[1])&&(num[0]));
     c=((~num[3])&&(~num[2])&&(num[1])&&(~num[0]))||((num[3])&&(num[2])&&(~num[0]))||((num[3])&&(num[2])&&(num[1]));
     d=((~num[3])&&(~num[2])&&(~num[1])&&(num[0]))||((~num[3])&&(num[2])&&(~num[1])&&(~num[0]))||((num[2])&&(num[1])&&(num[0]))||((num[3])&&(~num[2])&&(num[1])&&(~num[0]));
     e=((~num[3])&&(num[0]))||((~num[3])&&(num[2])&&(~num[1]))||((~num[2])&&(~num[1])&&(num[0]));
-    f=((~num[3])&&(~num[2])&&(num[0]))||((~num[3])&&(~num[2])&&(num[1]))||((~num[3])&&(num[2])&&(num[0]))||((num[3])&&(num[2])&&(~num[1])&&(num[0]));
+    f=((~num[3])&&(~num[2])&&(num[0]))||((~num[3])&&(~num[2])&&(num[1]))||((~num[3])&&(num[1])&&(num[0]))||((num[3])&&(num[2])&&(~num[1])&&(num[0]));
     g=((~num[3])&&(~num[2])&&(~num[1]))||((~num[3])&&(num[2])&&(num[1])&&(num[0]))||((num[3])&&(num[2])&&(~num[1])&&(~num[0]));
     a1=(sel[2])||(sel[1])||(sel[0]);
     a2=(sel[2])||(sel[1])||(~sel[0]);
